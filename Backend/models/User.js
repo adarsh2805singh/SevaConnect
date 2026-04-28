f6fe747735945fdf1f9938199ed565bd174db5c1
Nikhil <nikhilnagraju1@gmail.com>
2026-04-28 21:07:06 +0530

    Fix backend issues and remove duplicate controller

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -115,19 +115,14 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   // Only hash if password is modified
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    return next();
-  } catch (error) {
-    return next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method to compare passwords
